a1dafa8e630b64d187792c138d021b2717d9bd41
Alex Kelly <alex.kelly@william-lee.com>
2012-06-25 11:49:25 +0100

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -65,6 +65,7 @@ describe "UserPages" do
         
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
     
